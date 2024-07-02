a825cb238ec677c4ce8a1a56215c7ac8df55e356
Jeong-in <162551171+munginisfree@users.noreply.github.com>
2024-07-03 00:53:08 +0900

    # 상품 삭제 -> cancel_by_store_at 업데이트로 변경

--- src/main/java/org/nmfw/foodietree/domain/store/mapper/StoreMyPageMapper.java
+++ src/main/java/org/nmfw/foodietree/domain/store/mapper/StoreMyPageMapper.java
@@ -4,6 +4,7 @@ import org.apache.ibatis.annotations.Mapper;
 import org.nmfw.foodietree.domain.store.dto.resp.StoreMyPageDto;
 import org.nmfw.foodietree.domain.store.dto.resp.StoreReservationDto;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 @Mapper
@@ -15,5 +16,17 @@ public interface StoreMyPageMapper {
     // 가게 예약리스트 조회
     List<StoreReservationDto> findReservations(String storeId);
 
+    /**
+     * 상품 업데이트
+     * @param storeId
+     * @param pickupTime
+     */
+    void updateProductAuto(String storeId, LocalDateTime pickupTime);
 
+    /**
+     * 상품 삭제 -> 문 닫는 날에 오전 00시에 업데이트된 상품에 대해 cancel_by_store_at 업데이트
+     * @param storeId : 가게 아이디
+     * @param pickupTime : 문닫는 날 LocalDate.now().toString() 로 전달하거나 "yyyy-MM-dd" 형식으로 전달
+     */
+    void cancelProductByStore(String storeId, String pickupTime);
 }
