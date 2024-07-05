ab5f07b07fdf4ef7d031ce3b92cc7f3a0cff809f
hoseong511 <thdghtjd115@naver.com>
2024-07-06 08:48:14 +0900

    add lodash.js refactor utils

--- src/main/resources/static/assets/js/utils.js
+++
@@ -1,9 +0,0 @@
-export function debounce(callback, wait) { // closure
-  let timer;
-  return (...value) => {
-    clearTimeout(timer);
-    timer = setTimeout(() => {
-      callback(...value);
-    }, wait);
-  }
-}
