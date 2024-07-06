3b33abc3fac611c780630259eda7e35a19a1be5a
hoseong511 <thdghtjd115@naver.com>
2024-07-06 19:44:50 +0900

    feat email verifycode for signup
    - 회원가입시 이용할 controller, service 로직 추가

--- src/main/java/org/nmfw/foodietree/domain/auth/controller/EmailController.java
+++ src/main/java/org/nmfw/foodietree/domain/auth/controller/EmailController.java
@@ -47,10 +47,15 @@ public class EmailController {
     }
 
     @PostMapping("/verifyCode")
-    public ResponseEntity<?> verifyCode(@RequestBody Map<String, String> request) {
+    public ResponseEntity<?> verifyCode(@RequestBody Map<String, String> request, @RequestParam(required = false) String type) {
         String email = request.get("email");
         String code = request.get("code");
-        boolean isValid = emailService.verifyCode(email, code);
+        boolean isValid = false;
+        if (type != null && type.equals("signup")) {
+            isValid = emailService.verifyCodeForSignUp(email, code);
+        } else {
+            isValid = emailService.verifyCode(email, code);
+        }
         if (isValid) {
             return ResponseEntity.ok("success");
         } else {
