940b9cf44a5f19df4704342c889abc88d7929d4a
Jeong-in <162551171+munginisfree@users.noreply.github.com>
2024-07-06 14:49:24 +0900

    fix/email-subject

--- src/main/java/org/nmfw/foodietree/domain/auth/service/EmailService.java
+++ src/main/java/org/nmfw/foodietree/domain/auth/service/EmailService.java
@@ -29,7 +29,12 @@ public class EmailService {
         MimeMessage message = javaMailSender.createMimeMessage();
         MimeMessageHelper helper = new MimeMessageHelper(message, true, "UTF-8");
         helper.setTo(to);
-        helper.setSubject("비밀번호 재설정 인증 코드");
+
+        String subject = "FoodieTree 비밀번호 재설정 인증코드";
+        if(purpose.equalsIgnoreCase("signup")) {
+            subject = "FoodieTree 회원가입 인증코드";
+        }
+        helper.setSubject(subject);
 
         // 생성한 코드와 유효시간을 이메일 템플릿에 포함하여 HTML을 직접 작성
         String htmlContent = generateEmailHtml(code, purpose);
