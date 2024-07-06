c0a0272bedd9d70df1734f4479a0faa57ef82b0f
hoseong511 <thdghtjd115@naver.com>
2024-07-06 17:55:39 +0900

    undo fix sign in type

--- src/main/resources/static/assets/js/customer/sign-in.js
+++ src/main/resources/static/assets/js/customer/sign-in.js
@@ -9,7 +9,7 @@ export async function updatePassword(customerId, newPassword, newPasswordCheck) 
             headers: {
                 'Content-Type': 'application/json'
             },
-            body: JSON.stringify(newPassword),
+            body: JSON.stringify({ type: 'password', value: newPassword }),
         });
 
         if (response.ok) {
