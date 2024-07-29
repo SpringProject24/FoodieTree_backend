8fc4010da95b8b06e24148a5725cbc31cb32a517
dazzlingtea <kitsnr08@gmail.com>
2024-07-29 16:58:45 +0900

    # add FavFood repository

--- src/main/java/org/nmfw/foodietree/domain/customer/entity/FavFood.java
+++ src/main/java/org/nmfw/foodietree/domain/customer/entity/FavFood.java
@@ -20,7 +20,7 @@ public class FavFood {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "id")
-    private int id;
+    private Long id;
 
     @Column(name = "customer_id", nullable = false)
     private String customerId;
