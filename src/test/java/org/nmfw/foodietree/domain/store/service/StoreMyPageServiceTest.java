bd0c63bff9ce26a749950b49319ffc24e64e6a7d
Jeong-in <162551171+munginisfree@users.noreply.github.com>
2024-08-02 11:57:30 +0900

    WIP StoreMyPageServiceTest

--- src/test/java/org/nmfw/foodietree/domain/store/service/StoreMyPageServiceTest.java
+++ src/test/java/org/nmfw/foodietree/domain/store/service/StoreMyPageServiceTest.java
@@ -2,6 +2,8 @@ package org.nmfw.foodietree.domain.store.service;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.nmfw.foodietree.domain.store.dto.resp.StoreMyPageCalendarModalDto;
+import org.nmfw.foodietree.domain.store.dto.resp.StoreMyPageDto;
 import org.nmfw.foodietree.domain.store.dto.resp.StoreReservationDto;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -29,5 +31,30 @@ class StoreMyPageServiceTest {
             System.out.println("reservation.getPickupTime() = " + reservation.getPickedUpAt());
             System.out.println("reservation.getReservationTime() = " + reservation.getCancelReservationAt());
         }
+    }
+
+    @Test
+    @DisplayName("jpa로 전환 후 가게 정보 가져오기 테스트")
+    void getStoreInfoTest() {
+        //given
+        String storeId = "aaa@aaa.com";
+        //when
+        StoreMyPageDto storeMyPageInfo = storeMyPageService.getStoreMyPageInfo(storeId);
+
+        //then
+        System.out.println("storeMyPageInfo = " + storeMyPageInfo);
+    }
+
+    @Test
+    @DisplayName("jpa 전환 후 달력 상세 내역 가져오기 테스트")
+    void getCalendarData() {
+        //given
+        String storeId = "thdghtjd115@naver.com";
+        String date = "2024-08-01";
+        //when
+        StoreMyPageCalendarModalDto dto = storeMyPageService.getStoreMyPageCalendarModalInfo(storeId, date);
+
+        //then
+        System.out.println("dto = " + dto);
     }
 }
