247d5113319437baeb1163ca8f45c239ee809df4
dazzlingtea <kitsnr08@gmail.com>
2024-08-09 02:04:13 +0900

    WIP controller and service to update status

--- src/main/java/org/nmfw/foodietree/domain/admin/controller/AdminController.java
+++ src/main/java/org/nmfw/foodietree/domain/admin/controller/AdminController.java
@@ -2,6 +2,7 @@ package org.nmfw.foodietree.domain.admin.controller;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.nmfw.foodietree.domain.admin.dto.req.ApprovalStatusDto;
 import org.nmfw.foodietree.domain.admin.service.AdminService;
 import org.nmfw.foodietree.domain.store.dto.resp.ApprovalInfoDto;
 import org.springframework.data.domain.Page;
@@ -35,21 +36,21 @@ public class AdminController {
         return ResponseEntity.ok().body(approvalsMap);
     }
 
-    // 스토어 등록 요청을 승인
+    // 스토어 등록 요청 목록을 관리자가 검토 후 승인 또는 거절 처리
     @RequestMapping("/approve")
     @PostMapping
     public ResponseEntity<?> approveStore(
-            @RequestBody String storeId,
+            @RequestBody ApprovalStatusDto dto,
             @AuthenticationPrincipal TokenUserInfo userInfo
     ) {
 
+        int resultCnt;
         try {
-            adminService.sendStoreInfo(storeId, userInfo);
+            resultCnt = adminService.updateApprovalsStatus(dto, userInfo);
         } catch (Exception e) { // 예외처리 보완 필요
             return ResponseEntity.badRequest().body(e.getMessage());
         }
-
-        return ResponseEntity.ok().body("");
+        return ResponseEntity.ok().body(Map.of("result", resultCnt));
     }
 
 }
--- src/main/java/org/nmfw/foodietree/domain/admin/service/AdminService.java
+++ src/main/java/org/nmfw/foodietree/domain/admin/service/AdminService.java
@@ -2,9 +2,10 @@ package org.nmfw.foodietree.domain.admin.service;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.nmfw.foodietree.domain.admin.dto.req.ApprovalStatusDto;
+import org.nmfw.foodietree.domain.admin.dto.res.StoreApproveDto;
 import org.nmfw.foodietree.domain.auth.security.TokenProvider;
 import org.nmfw.foodietree.domain.store.dto.resp.ApprovalInfoDto;
-import org.nmfw.foodietree.domain.store.entity.Store;
 import org.nmfw.foodietree.domain.store.entity.StoreApproval;
 import org.nmfw.foodietree.domain.store.entity.value.ApproveStatus;
 import org.nmfw.foodietree.domain.store.repository.StoreApprovalRepository;
@@ -12,14 +13,12 @@ import org.nmfw.foodietree.domain.store.repository.StoreRepository;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
-import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-import java.util.NoSuchElementException;
 import java.util.stream.Collectors;
 
 import static org.nmfw.foodietree.domain.auth.security.TokenProvider.*;
@@ -51,33 +50,62 @@ public class AdminService {
     }
 
     /**
-     * 스토어 등록 요청을 승인하면 store 업데이트
-     * @param storeId - 승인된 스토어 계정
+     * 스토어 등록 요청 리스트를 한 번에 승인, 거절 처리
+     * @param dto - 액션타입(승인, 거절)과 요청 Id 목록
      * @param userInfo - 관리자 정보를 담은 토큰
      */
-    public void sendStoreInfo(
-            String storeId,
+    public int updateApprovalsStatus(
+            ApprovalStatusDto dto,
             TokenUserInfo userInfo
     ) {
         // 관리자가 아닌 경우 BadRequest
-        if(!userInfo.getRole().equals("ADMIN")) {
-            throw new RuntimeException("관리자 권한이 없습니다.");
+//        if(!userInfo.getRole().equals("admin")) {
+//            throw new RuntimeException("관리자 권한이 없습니다.");
+//        }
+        ApproveStatus status = ApproveStatus.valueOf(dto.getActionType().toUpperCase());
+
+        List<Long> approvalIdList = dto.getApprovalIdList();
+        Long result = storeApprovalRepository.updateApprovalStatus(status, approvalIdList);
+        int updateCnt = result.intValue();
+
+        if(status.equals(ApproveStatus.APPROVED)) { // 승인 요청인 경우 store 업데이트
+            updateCnt = sendStoreInfo(approvalIdList, userInfo);
         }
-
-        StoreApproval foundApproval = storeApprovalRepository.findByStoreId(storeId);
-
-        Store foundStore = storeRepository.findByStoreId(foundApproval.getStoreId())
-                .orElseThrow(()->new NoSuchElementException("존재하지 않는 스토어입니다."));
-
-        if(foundApproval.getStatus() == ApproveStatus.APPROVED
-                || foundApproval.getStatus() == ApproveStatus.REJECTED) {
-            throw new RuntimeException("이미 검토한 요청입니다.");
-        }
-        // store approve 상태를 APPROVED, storeApproval 정보 setter로 업데이트
-        Store updatedStore = foundApproval.updateFromStoreApproval(foundStore);
-
-        Store saved = storeRepository.save(updatedStore);
-        log.info("saved store: {}", saved);
+        return updateCnt;
     }
+    /**
+     * 스토어 등록 요청을 승인하면 store 업데이트
+     * @param ids - 승인된 요청 Id 목록
+     * @param userInfo - 관리자 정보를 담은 토큰
+     */
+    public int sendStoreInfo(
+            List<Long> ids,
+            TokenUserInfo userInfo
+    ) {
+        // 엔터티를 DTO로 변환
+        List<StoreApproval> foundApprovals = storeApprovalRepository.findAllByIdInIds(ids);
+        List<StoreApproveDto> dtos = foundApprovals.stream()
+                .map(StoreApproveDto::new)
+                .collect(Collectors.toList());
+
+        // DTO를 사용하여 업데이트 수행
+        Long result = storeApprovalRepository.updateStoreInfo(dtos);
+
+        return result.intValue();
+
+    }
+//        // 단일 업데이트
+//        Store foundStore = storeRepository.findByStoreId(foundApproval.getStoreId())
+//                .orElseThrow(()->new NoSuchElementException("존재하지 않는 스토어입니다."));
+//
+//        if(foundApproval.getStatus() == ApproveStatus.APPROVED
+//                || foundApproval.getStatus() == ApproveStatus.REJECTED) {
+//            throw new RuntimeException("이미 검토한 요청입니다.");
+//        }
+//        // store approve 상태를 APPROVED, storeApproval 정보 setter로 업데이트
+//        Store updatedStore = foundApproval.updateFromStoreApproval(foundStore);
+//
+//        Store saved = storeRepository.save(updatedStore);
+//        log.info("saved store: {}", saved);
 
 }
