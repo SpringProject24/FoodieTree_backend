58e0b484813abdedfc34cc586f8ae9ac14b5e8f6
hoseong511 <thdghtjd115@naver.com>
2024-08-13 17:00:55 +0900

    init favStoreList

--- src/main/java/org/nmfw/foodietree/domain/store/repository/FavStoreRepositoryCustom.java
+++ src/main/java/org/nmfw/foodietree/domain/store/repository/FavStoreRepositoryCustom.java
@@ -3,7 +3,11 @@ package org.nmfw.foodietree.domain.store.repository;
 import org.nmfw.foodietree.domain.customer.entity.FavStore;
 
 import java.util.List;
+import org.nmfw.foodietree.domain.store.dto.resp.FavStoreDto;
+import org.nmfw.foodietree.domain.store.dto.resp.StoreListDto;
 
 public interface FavStoreRepositoryCustom {
-    List<FavStore> findFavStoresOrReservations(String customerId);
+    List<StoreListDto> findFavStoresByCustomerId(String customerId);
+    List<FavStoreDto> findFavStoresByOrders(String customerId);
+
 }
---
+++ src/test/java/org/nmfw/foodietree/domain/store/repository/FavStoreRepositoryCustomImplTest.java
@@ -0,0 +1,20 @@
+package org.nmfw.foodietree.domain.store.repository;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+
+@SpringBootTest
+@Transactional
+class FavStoreRepositoryCustomImplTest {
+
+	@Test
+	void findFavStoresByCustomerId() {
+	}
+
+	@Test
+	void findFavStoresByOrders() {
+	}
+}
