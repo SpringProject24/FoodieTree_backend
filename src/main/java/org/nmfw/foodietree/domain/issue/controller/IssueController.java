11c0cd72be055579af7fb853c2e22b4aac02828d
Jeong-in <162551171+munginisfree@users.noreply.github.com>
2024-08-20 19:20:05 +0900

    WIP SAVE ISSUE

--- src/main/java/org/nmfw/foodietree/domain/issue/controller/IssueController.java
+++ src/main/java/org/nmfw/foodietree/domain/issue/controller/IssueController.java
@@ -3,6 +3,7 @@ package org.nmfw.foodietree.domain.issue.controller;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.nmfw.foodietree.domain.issue.dto.res.IssueDto;
+import org.nmfw.foodietree.domain.issue.entity.Issue;
 import org.nmfw.foodietree.domain.issue.repository.IssueRepository;
 import org.nmfw.foodietree.domain.issue.service.IssueService;
 import org.springframework.http.ResponseEntity;
@@ -34,10 +35,21 @@ public class IssueController {
 
     @PostMapping
     public ResponseEntity<?> issue(@RequestBody Map<String, String> issue) {
-        log.info("save issues");
-        log.info("issue : {}", issue);
-        issueService.saveWithCategory(issue.get("customerId"), issue.get("issueCategor"));
-        return ResponseEntity.ok().body("success");
+        String customerId = issue.get("customerId");
+        String reservationId = issue.get("reservationId");
+        log.info("customerId : {}", customerId);
+        log.info("reservationId : {}", reservationId);
+        Issue newIssue = Issue.builder()
+                .customerId(customerId)
+                .reservationId(Integer.parseInt(reservationId))
+                .cancelIssueAt(null)
+                .issueCategory("")
+                .issueCompleteAt(null)
+                .issueText("")
+                .makeIssueAt(null)
+                .build();
+        Issue save = issueRepository.save(newIssue);
+        return ResponseEntity.ok().body(save.getIssueId());
     }
 
 }
---
+++ src/test/java/org/nmfw/foodietree/domain/issue/controller/IssueControllerTest.java
@@ -0,0 +1,34 @@
+package org.nmfw.foodietree.domain.issue.controller;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.ResponseEntity;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Map;
+
+import static org.junit.jupiter.api.Assertions.*;
+@Transactional
+@SpringBootTest
+//@RequiredConstructor
+class IssueControllerTest {
+
+    private static final Logger log = LoggerFactory.getLogger(IssueControllerTest.class);
+    @Autowired
+    private IssueController issueController;
+
+    @Test
+    @DisplayName("이슈 저장 테스트")
+    void saveTest() {
+        //given
+        Map<String, String> issue = Map.of("customerId", "test@gmail.com", "reservationId", "38");
+        //when
+        ResponseEntity<?> issue1 = issueController.issue(issue);
+        //then
+        System.out.println("issue1 = " + issue1);
+    }
+}
