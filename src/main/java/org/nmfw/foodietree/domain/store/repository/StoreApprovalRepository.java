c0426b1de845d9c0d8c86cfafafc5d3e4ea0710e
dazzlingtea <kitsnr08@gmail.com>
2024-08-22 16:58:12 +0900

    WIP refactoring store approval to update recent approval

--- src/main/java/org/nmfw/foodietree/domain/store/repository/StoreApprovalRepository.java
+++ src/main/java/org/nmfw/foodietree/domain/store/repository/StoreApprovalRepository.java
@@ -8,5 +8,4 @@ import java.util.List;
 public interface StoreApprovalRepository
         extends JpaRepository<StoreApproval, Long>, StoreApprovalRepositoryCustom {
 
-    StoreApproval findByStoreId(String storeId);
 }
